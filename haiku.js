295a331727a93f8104736cabd1a823cd63e10427
tessaSAC <tessaSAC@users.noreply.github.com>
2016-07-22 17:24:16 -0400

    current program

--- haiku.js
+++ haiku.js
@@ -12,20 +12,29 @@ function formatData(data) {
 	var lineSplit;
 	lines.forEach(function(line){
    		lineSplit = line.split("  ");
-   		// Record syllable length;
-   		lineSplit[1] = lineSplit[1].match(/\d/g).length;
-    	console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]);
 
- 		// Split into sorted arrays
+
+   		// RECORD SYLLABLE LENGTH:
+   		if ((/\d/).test(lineSplit[1])) {
+   			lineSplit[1] = lineSplit[1].match(/\d/g).length;
+   		}
+    	// console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]);
+
+
+ 		// SPLIT INTO SORTED ARRAYS
+ 		var thisWord = lineSplit[0].replace(/[^A-Z]/g, "");
  		switch (lineSplit[1]) {
  			case 1:
- 				arr1.push(lineSplit[1]);
+ 				if (arr1.indexOf(thisWord) === -1)
+ 					{arr1.push(thisWord);}
  				break;
  			case 2:
- 				arr2.push(lineSplit[2]);
+	 			if (arr2.indexOf(thisWord) === -1)
+	 				{arr2.push(thisWord);}
  				break;
  			case 3:
- 				arr3.push(lineSplit[3]);
+	 			if (arr3.indexOf(thisWord) === -1)
+	 				{arr3.push(thisWord);}
  				break;
 		}
 	});
@@ -34,12 +43,37 @@ function formatData(data) {
 formatData(cmudictFile);
 
 
-console.log(arr1, arr2, arr3);
-
-
 
 function createHaiku(structure){
-    console.log("this should log a haiku with the structure " + structure);
+	var haiku = "",
+		thisLine = "",
+		thisArr;
+    for (var i = 0; i < structure.length; i++) {
+    	var lineCount = structure[i],
+    		whichArr = [];
+    	while (lineCount > 0) {
+    		// SELECT ARRAY
+    		switch (lineCount) {
+    			case 3:
+    				whichArr.push(arr3); // fall through
+    			case 2:
+    				whichArr.push(arr2); // fall through
+    			case 1:
+    				whichArr.push(arr1);
+    				thisArr = whichArr[Math.floor(Math.random()*whichArr.length)];
+			}
+			// ADJUST LINE COUNT
+			switch (thisArr) {
+				case arr1:
+					--lineCount;
+				case arr2:
+					--lineCount;
+				case arr3:
+					--lineCount
+			}
+			// add a random word to the line
+    	}
+    }
 }
 
 module.exports = {
